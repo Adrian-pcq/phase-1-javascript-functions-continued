de77893a46756908e3af1f854881ca9f8707a9bc
Adrian <adrianqueizan@gmail.com>
2023-08-21 16:40:59 -0400

    done functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(value){
+if (value){
+    return `This Saturday, I want to ${value}!`
+}else {return "This Saturday, I want to roller-skate!"}
+}
+saturdayFun("bath my dog");
+
+function mondayWork(value){
+    if(value){
+        return `This Monday, I will ${value}.`
+    } else {return "This Monday, I will go to the office."}
+}
+
+function wrapAdjective(value = '*'){
+    return function (who= "a hard worker"){
+        return `You are ${value}${who}${value}!`
+    }
+}
